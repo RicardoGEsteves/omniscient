943006807fe47f93fee13f97ac2ded0d38be9b49
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-11-28 23:31:02 +0000

    feat: :sparkles: Create video generation
    - Implement video generation with respective structure and implementation
     -Add prisma to the project - initial prisma setup
    
    Video generation created

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -5,8 +5,9 @@ import axios from "axios";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+
+import { FileAudio } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { Music, Send } from "lucide-react";
 
 import Heading from "@/components/heading";
 import { Button } from "@/components/ui/button";
@@ -17,9 +18,10 @@ import { Empty } from "@/components/empty";
 
 import { formSchema } from "./constants";
 
-const MusicPage = () => {
+const VideoPage = () => {
   const router = useRouter();
-  const [music, setMusic] = useState<string>();
+
+  const [video, setVideo] = useState<string>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -32,11 +34,11 @@ const MusicPage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      setMusic(undefined);
+      setVideo(undefined);
 
-      const response = await axios.post("/api/music", values);
+      const response = await axios.post("/api/video", values);
 
-      setMusic(response.data.audio);
+      setVideo(response.data[0]);
       form.reset();
     } catch (error: any) {
       if (error?.response?.status === 403) {
@@ -50,11 +52,11 @@ const MusicPage = () => {
   return (
     <div>
       <Heading
-        title="Music Generation"
-        description="Turn your prompt into music."
-        icon={Music}
-        iconColor="text-emerald-500"
-        bgColor="bg-emerald-500/10"
+        title="Video Generation"
+        description="Turn your prompt into video."
+        icon={FileAudio}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
       />
       <div className="px-4 lg:px-8">
         <Form {...form}>
@@ -81,7 +83,7 @@ const MusicPage = () => {
                     <Input
                       className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                       disabled={isLoading}
-                      placeholder="Piano solo"
+                      placeholder="Clown fish swimming in a coral reef"
                       {...field}
                     />
                   </FormControl>
@@ -103,15 +105,18 @@ const MusicPage = () => {
             <Loader />
           </div>
         )}
-        {!music && !isLoading && <Empty label="No music generated." />}
-        {music && (
-          <audio controls className="w-full mt-8">
-            <source src={music} />
-          </audio>
+        {!video && !isLoading && <Empty label="No video files generated." />}
+        {video && (
+          <video
+            controls
+            className="w-full aspect-video mt-8 rounded-lg border bg-black"
+          >
+            <source src={video} />
+          </video>
         )}
       </div>
     </div>
   );
 };
 
-export default MusicPage;
+export default VideoPage;
---
+++ app/api/video/route.ts
@@ -0,0 +1,37 @@
+import Replicate from "replicate";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN!,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt is required", { status: 400 });
+    }
+
+    const response = await replicate.run(
+      "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
+      {
+        input: {
+          prompt,
+        },
+      }
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
